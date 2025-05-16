305cf544e1ad1bd448dcd1e1a1fec966f9f54ba8
AnzhelikaZhalynbekova <anjiko333@gmail.com>
2025-05-16 10:20:11 +0600

    user entity

---
+++ src/main/java/com/StudyHub/StudyHub/model/User.java
@@ -0,0 +1,36 @@
+package com.StudyHub.StudyHub.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // автоинкремент id
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)  // храним роли как список строк
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles;
+
+    private boolean enabled = false;  // по умолчанию пользователь не активен (нужно подтвердить email)
+}
