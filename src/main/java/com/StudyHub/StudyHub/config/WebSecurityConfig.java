678517e5a5ebe132ea83563851936d203d2a0729
AnzhelikaZhalynbekova <anjiko333@gmail.com>
2025-05-16 15:17:22 +0600

    swagger improvs

--- src/main/java/com/StudyHub/StudyHub/config/WebSecurityConfig.java
+++ src/main/java/com/StudyHub/StudyHub/config/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class WebSecurityConfig {
@@ -35,6 +36,7 @@ public class WebSecurityConfig {
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
+                .httpBasic(withDefaults())
 
                 .userDetailsService(userDetailsService)
                 .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
